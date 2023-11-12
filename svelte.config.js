46e664e16c9b516bcd487ac3c8726593bdd8f54f
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-11-12 20:31:25 +0000

    Initialize v3.0.0_neoapi

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import path from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			$e: path.resolve('./src/env.server.ts'),
+			$lib: path.resolve('./src/lib'),
+			$api: path.resolve('./src/server')
+		}
+	}
+};
+
+export default config;
